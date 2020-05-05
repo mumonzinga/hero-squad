253114947b5034f7944bed5fe002da8d75e367d1
Mumo <mnzinga888@gmail.com>
2020-05-05 06:52:10 +0300

    add more herodao tests

--- src/test/java/dao/Sql2oHeroDaoTest.java
+++ src/test/java/dao/Sql2oHeroDaoTest.java
@@ -30,6 +30,57 @@ public class Sql2oHeroDaoTest {
         assertNotEquals(originalHeroId, hero.getId());
     }
 
+    @Test
+    public void existingHeroesCanBeFoundById() throws Exception {
+        Hero hero = setUpNewHero();
+        heroDao.add(hero); //add to dao (takes care of saving)
+        Hero foundHero = heroDao.findById(hero.getId()); //retrieve
+        assertEquals(hero, foundHero); //should be the same
+    }
+
+    @Test
+    public void addedHeroesAreReturnedFromgetAll() throws Exception {
+        Hero hero = setUpNewHero();
+        heroDao.add(hero);
+        assertEquals(1, heroDao.getAll().size());
+    }
+
+    @Test
+    public void noHeroesReturnsEmptyList() throws Exception {
+        assertEquals(0, heroDao.getAll().size());
+    }
+
+    @Test
+    public void updateChangesHeroContent() throws Exception {
+        String initialName = "EL";
+        Hero hero = setUpNewHero();
+        heroDao.add(hero);
+
+        heroDao.update(hero.getId(),"MC", 30, "mari", "mari");
+        Hero updatedHero = heroDao.findById(hero.getId()); //why do I need to refind this?
+        assertNotEquals(initialName, updatedHero.getName());
+    }
+
+    @Test
+    public void deleteByIdDeletesCorrectHero() throws Exception {
+        Hero hero = setUpNewHero();
+        heroDao.add(hero);
+        heroDao.deleteById(hero.getId());
+        assertEquals(0, heroDao.getAll().size());
+    }
+
+    @Test
+    public void clearAllClearsAll() throws Exception {
+        Hero hero = setUpNewHero();
+        Hero otherHero = new Hero("MC", 30, "mari", "mari");
+        heroDao.add(hero);
+        heroDao.add(otherHero);
+        int daoSize = heroDao.getAll().size();
+        heroDao.clearAllHeroes();
+        assertTrue(daoSize > 0 && daoSize > heroDao.getAll().size()); //this is a little overcomplicated, but illustrates well how we might use `assertTrue` in a different way.
+    }
+
+
     public Hero setUpNewHero() {
         return new Hero("EL", 30, "madem", "madem");
     }
